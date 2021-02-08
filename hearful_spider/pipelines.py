8b401d8d260b26a82308320eb19573f972151c35
hfolk25 <hfolk25@gmail.com>
2021-02-08 11:27:43 -0800

    getting all the reviews stored in mongoDB

--- hearful_spider/pipelines.py
+++ hearful_spider/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
+
 
 class HearfulSpiderPipeline(object):
+    def __init__(self):
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+        db = self.conn['gopro']
+        self.collection = db['reviews']
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
