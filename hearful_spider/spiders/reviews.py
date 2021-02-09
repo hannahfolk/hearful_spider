9705e471006156050dc3e95bba6f2465b5c51968
hfolk25 <hfolk25@gmail.com>
2021-02-08 19:01:41 -0800

    adding product_id key to each review dict and custom settings

--- hearful_spider/spiders/reviews.py
+++ hearful_spider/spiders/reviews.py
@@ -4,6 +4,9 @@ import scrapy
 class ReviewsSpider(scrapy.Spider):
     """ This spider scrapes the GoPro Fusion Waterproof Digital Spherical Amazon page for all reviews. """
     name = "reviews"
+    custom_settings = {
+        "COLLECTION_NAME": "reviews",
+    }
     start_urls = [
         "https://www.amazon.com/GoPro-Fusion-Waterproof-Digital-Spherical/product-reviews/B0792MJLNM/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews",
     ]
@@ -15,7 +18,9 @@ class ReviewsSpider(scrapy.Spider):
                 "review_title": review.css("a.review-title span::text").get(),
                 "review_date": review.css("span.review-date::text").get(),
                 "review_star_rating": review.css("i.review-rating span::text").get(),
-                "review_text": review.css("span.review-text span::text").getall()
+                "review_text": review.css("span.review-text span::text").getall(),
+                # below id is copied from mongoDB collection 'products'
+                "product_id": "6021fa56f08cd707597d49d8",
             }
 
         next_page = response.css("li.a-last a::attr(href)").get()
