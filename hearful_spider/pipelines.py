cef3788dd10f52140becacba37f1eaf11e2fcb57
hfolk25 <hfolk25@gmail.com>
2021-02-08 19:01:04 -0800

    modifying pipeline to fit multiple collections

--- hearful_spider/pipelines.py
+++ hearful_spider/pipelines.py
@@ -9,14 +9,24 @@ import pymongo
 
 
 class HearfulSpiderPipeline(object):
-    def __init__(self):
-        self.conn = pymongo.MongoClient(
-            'localhost',
-            27017
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
-        db = self.conn['gopro']
-        self.collection = db['reviews']
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
+        self.db[spider.settings.get("COLLECTION_NAME")].insert_one(dict(item))
         return item
